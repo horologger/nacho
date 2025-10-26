b9f480a7e8ddc16e80de2e7dbc493d7d461f8414
Alex Tsokurov <me@ximik.net>
2025-10-26 17:16:15 +0100

    upgrade and fix

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,6 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.unstable_enableSymlinks = true;
 config.resolver.unstable_enablePackageExports = true;
 
 config.transformer.getTransformOptions = async () => ({
